6be5cbad8cec197ecef0eceb427a72e6ecfbc6f8
George Mukha <georgymukha@yandex.ru>
2024-07-14 23:55:54 +0500

    git init

---
+++ src/main/kotlin/com/mukha/smartmoney/SmartmoneyApplication.kt
@@ -0,0 +1,11 @@
+package com.mukha.smartmoney
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartmoneyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmartmoneyApplication>(*args)
+}
---
+++ src/test/kotlin/com/mukha/smartmoney/SmartmoneyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mukha.smartmoney
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartmoneyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
